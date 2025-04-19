d96dd31e82070d781504d5134e00874c40f9b2b4
Almir Sharifullin <a.sharifullinx@dodobrands.io>
2025-04-19 14:29:15 +0500

    dataprovider + postgresql composes

---
+++ DataProvider/Migrations/2025/04/202504191340_initialize.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+
+namespace DataProvider.Migrations._2025._04
+{
+    [Migration(202504191340)]
+    public class _202504191340_initialize : Migration
+    {
+        private string TableName => "Users";
+        public override void Up()
+        {
+            Create.Table(TableName)
+                .WithColumn("Id").AsGuid().PrimaryKey()
+                .WithColumn("Email").AsString(256).NotNullable()
+                .WithColumn("Username").AsString(128).NotNullable()
+                .WithColumn("Password").AsString(128).NotNullable()
+                .WithColumn("RefreshToken").AsString(512).NotNullable()
+                .WithColumn("IsActive").AsBoolean().NotNullable().WithDefaultValue(true)
+                .WithColumn("Comment").AsString(256).NotNullable()
+                .WithColumn("Created").AsDateTime().NotNullable()
+                .WithColumn("Modified").AsDateTime().Nullable();
+
+        }
+
+        public override void Down()
+        {
+            Delete.Table(TableName);
+        }
+    }
+}
