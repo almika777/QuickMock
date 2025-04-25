7085572b48fecfdb8f8b14b72bca8f2117198ea6
Almir Sharifullin <a.sharifullinx@dodobrands.io>
2025-04-26 00:47:36 +0500

    tests and repositories

--- DataProvider/AppDbContext.cs
+++ DataProvider/AppDbContext.cs
@@ -1,13 +1,17 @@
-﻿using LinqToDB.Data;
+﻿using DataProvider.Entities;
+using LinqToDB;
+using LinqToDB.Data;
 using LinqToDB.DataProvider.PostgreSQL;
 
 namespace DataProvider
 {
-    internal class AppDbContext : DataConnection
+    public class AppDbContext : DataConnection
     {
         public AppDbContext(string connectionString)
             : base(PostgreSQLTools.GetDataProvider(PostgreSQLVersion.v15), connectionString)
         {
         }
+
+        public ITable<UserEntity> Users => this.GetTable<UserEntity>();
     }
 }
---
+++ DataProvider/Entities/UserEntity.cs
@@ -0,0 +1,35 @@
+﻿using LinqToDB.Mapping;
+
+namespace DataProvider.Entities
+{
+    [Table(Name = "Users")]
+    public class UserEntity
+    {
+        [Column]
+        public Guid Id { get; set; }
+        
+        [Column]
+        public string Email { get; set; }
+        
+        [Column]
+        public string Username { get; set; }
+        
+        [Column]
+        public string Password { get; set; }
+        
+        [Column]
+        public string RefreshToken { get; set; }
+        
+        [Column]
+        public bool IsActive { get; set; }
+        
+        [Column]
+        public string Comment { get; set; }
+        
+        [Column]
+        public DateTime Created { get; set; }
+        
+        [Column]
+        public DateTime Modified { get; set; }
+    }
+}
