0f4a795bd83a0d5ae426468f33fda1a0c989053c
Almir Sharifullin <a.sharifullinx@dodobrands.io>
2025-05-26 23:31:57 +0500

    final + docker

---
+++ QuickMock/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,70 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace QuickMock.Middlewares;
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+    private readonly IHostEnvironment _env;
+
+    public ExceptionMiddleware(
+        RequestDelegate next,
+        ILogger<ExceptionMiddleware> logger,
+        IHostEnvironment env)
+    {
+        _next = next;
+        _logger = logger;
+        _env = env;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var response = _env.IsDevelopment()
+            ? new ApiException(
+                context.Response.StatusCode,
+                exception.Message,
+                exception.StackTrace)
+            : new ApiException(
+                context.Response.StatusCode,
+                "Internal Server Error");
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        };
+
+        var json = JsonSerializer.Serialize(response, options);
+        await context.Response.WriteAsync(json);
+    }
+}
+public class ApiException
+{
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+    public string? Details { get; set; }
+
+    public ApiException(int statusCode, string message, string? details = null)
+    {
+        StatusCode = statusCode;
+        Message = message;
+        Details = details;
+    }
+}
