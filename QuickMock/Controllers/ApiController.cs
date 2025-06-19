6467c1d767f592543c637ae0bef2fed775a3cb9c
Almir Sharifullin <a.sharifullinx@dodobrands.io>
2025-06-19 13:58:07 +0500

    fixed path generation

--- QuickMock/Controllers/ApiController.cs
+++ QuickMock/Controllers/ApiController.cs
@@ -6,11 +6,12 @@ namespace QuickMock.Controllers
     [ApiController]
     public class ApiController(IRequestProviderService requestProvider) : ControllerBase
     {
-        [Route("api/{*path}")]
+        [Route("api/{**path}")]
         [HttpGet, HttpPost]
-        public async Task<IActionResult> HandleRequest([FromRoute] string path)
+        public async Task<IActionResult> HandleRequest(string path)
         {
-            var request = await requestProvider.GetRequestValue(path);
+            var fullPath = $"{path}{HttpContext.Request.QueryString}";
+            var request = await requestProvider.GetRequestValue(fullPath);
             return request.Value != null
                 ? Ok(request.Value)
                 : BadRequest("Request was not found");
